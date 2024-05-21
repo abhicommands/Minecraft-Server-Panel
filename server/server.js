bba08d3b5870916e051a94235c2f06c00ce915f4
Abhinav Acharya <abhinavacharya@Abhinavs-MacBook-Pro.local>
2024-05-21 12:58:50 -0400

    finalized production build and setup a produciton env

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ const http = require("http");
 const cookie = require("cookie");
 
 const app = express();
-app.set("trust proxy", 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -30,7 +29,7 @@ app.use(
   })
 );
 app.use(cookieParser());
-
+app.set("trust proxy", 1);
 const server = http.createServer(app);
 const io = socket(server, {
   cors: {
@@ -39,7 +38,6 @@ const io = socket(server, {
     credentials: true, // Important for sending cookies and headers
   },
 });
-
 let terminals = {};
 
 const createTerminal = (logDir, startupCommand) => {
@@ -340,6 +338,7 @@ io.use((socket, next) => {
 });
 io.on("connection", (socket) => {
   socket.join(socket.serverId);
+  console.log("Socket connected:", socket.id);
   const terminal = terminals[socket.serverId];
   socket.on("command", (data) => {
     if (terminal && terminal.isServerRunning) {
@@ -393,6 +392,7 @@ io.on("connection", (socket) => {
     }
   });
   socket.on("disconnect", () => {
+    console.log("Socket disconnected:", socket.id);
     socket.disconnect();
   });
   socket.on("error", (error) => {
