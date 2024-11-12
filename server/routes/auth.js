50e44116f90c84a3652c32cf3c1b117582cf2e75
abhicommands <ac.abhinav@outlook.com>
2024-11-11 23:44:33 -0500

    small change in the authentication code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 const Joi = require("joi");
-const { db } = require("../db/db.js");
 
 const loginSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
@@ -16,11 +15,17 @@ const loginSchema = Joi.object({
 
 const router = express.Router();
 const secureStatus = process.env.SECURE_STATUS === "true" ? true : false;
+const user = {
+  admin: {
+    username: process.env.ROOT_USERNAME,
+    password: process.env.ROOT_PASSWORD_HASH,
+  },
+};
 
 const authenticateSocket = (token, callback) => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    callback(null, { username: decoded.username, role: decoded.role });
+    callback(null, { username: decoded.username });
   } catch (error) {
     callback(new Error("Authentication error"));
   }
@@ -30,7 +35,7 @@ const authenticate = (req, res, next) => {
   try {
     const token = req.cookies.token;
     const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = { username: decoded.username, role: decoded.role };
+    req.user = { username: decoded.username };
     next();
   } catch (error) {
     if (req.cookies.token) {
@@ -50,29 +55,22 @@ router.post("/login", async (req, res) => {
     return res.status(400).json({ error: error.details[0].message });
   }
   const { username, password } = value; // Using sanitized and validated inputs
-  db.get("SELECT * FROM users WHERE username = ?", [username], (err, user) => {
-    if (err) {
-      return res.status(500).json({ error: "Database error" });
-    }
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const token = jwt.sign(
-        { username: user.username, role: user.role },
-        JWT_SECRET,
-        { expiresIn: "7d" }
-      );
-      res.cookie("token", token, {
-        httpOnly: true,
-        secure: secureStatus,
-        sameSite: "strict",
-        maxAge: 604800000,
-      });
-      res.json({ message: "Login successful" });
-    } else {
-      res.status(401).json({ error: "Invalid username or password" });
-    }
-  });
+  if (
+    username === user.admin.username &&
+    bcrypt.compareSync(password, user.admin.password)
+  ) {
+    const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "7d" });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: secureStatus,
+      sameSite: "strict",
+      maxAge: 604800000,
+    });
+    res.json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ error: "Invalid username or password" });
+  }
 });
-
 router.post("/logout", (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
