a81ad992debb898eb92d61498881949f7d6e30b2
abhicommands <ac.abhinav@outlook.com>
2024-11-12 16:39:55 -0500

    changes to terminal log file truncation logic

--- server/utils/terminal.js
+++ server/utils/terminal.js
@@ -21,19 +21,40 @@ const createTerminal = (logDir, startupCommand, isMsh) => {
   let isServerRunning = false; // Ensure the log directory exists
   return { ptyProcess, isServerRunning, startupCommand, serverPID, isMsh };
 };
+
+const truncateLogFile = (logFilePath, maxLines = 1000) => {
+  try {
+    const data = fs.readFileSync(logFilePath, "utf8");
+    const lines = data.split("\n");
+    if (lines.length > maxLines) {
+      const truncatedData = lines.slice(-maxLines).join("\n");
+      fs.writeFileSync(logFilePath, truncatedData, "utf8");
+    }
+  } catch (err) {
+    console.error("Error truncating log file:", err);
+  }
+};
+
 const initializeTerminal = (io, serverId, terminalPty, logDir) => {
-  const logFile = fs.createWriteStream(path.join(logDir, "server.log"), {
-    flags: "a",
-  });
+  const logFilePath = path.join(logDir, "server.log");
+  const logFile = fs.createWriteStream(logFilePath, { flags: "a" });
   fs.removeSync(path.join(logDir, "../minecraft_pid.txt"));
+
   terminalPty.ptyProcess.on("data", function (rawOutput) {
-    if (io.sockets.adapter.rooms.get(serverId))
+    if (io.sockets.adapter.rooms.get(serverId)) {
       io.to(serverId).emit("output", rawOutput);
+    }
+
     logFile.write(rawOutput);
-    if (fs.existsSync(path.join(logDir, "../minecraft_pid.txt")))
+
+    // Check and truncate log file if it exceeds the max line count
+    truncateLogFile(logFilePath);
+
+    if (fs.existsSync(path.join(logDir, "../minecraft_pid.txt"))) {
       terminalPty.serverPID = parseInt(
         fs.readFileSync(path.join(logDir, "../minecraft_pid.txt"), "utf8")
       );
+    }
     if (terminalPty.serverPID) {
       try {
         process.kill(terminalPty.serverPID, 0);
